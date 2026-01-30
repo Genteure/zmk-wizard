be463cd801e0d59589b893d349aa6c51fee33811
genteure <genteure@gmail.com>
2026-01-30 11:34:35 +0800

    delete old files

--- src/components/app.tsx
+++
@@ -1,3 +0,0 @@
-// Re-export all app components from the app directory
-// This file is kept for backward compatibility
-export { App, ensureKeyIds } from "./app/index";
--- src/components/navbar.tsx
+++
@@ -1,3 +0,0 @@
-// Re-export all navbar components from the navbar directory
-// This file is kept for backward compatibility
-export { BuildButton, HelpButton, InfoEditButton } from "./navbar/index";
